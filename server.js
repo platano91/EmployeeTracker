e3c27861a368ea48f68fdced74cb26530415ec9b
Samuel <salvadomo@hotmail.com>
2024-01-17 14:00:36 -0700

    connection to db

--- server.js
+++ server.js
@@ -4,10 +4,17 @@ const mysql = require("mysql2");
 const dotenv = require('dotenv').config();
 const consoleTable = require("console.table");
 
-// Create MySQL connection using credentials from .env file
+// MySQL connection using credentials from .env
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
+});
+
+// Connection to database
+connection.connect(err => {
+    if (err) throw err;
+    console.log('Connected to the Employee Tracker database.');
+    mainMenu();
 });

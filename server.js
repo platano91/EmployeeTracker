e9fe9146d429bcd5d0e01272616a33545a85681d
Samuel <salvadomo@hotmail.com>
2024-01-17 14:03:34 -0700

    init main menu

--- server.js
+++ server.js
@@ -18,3 +18,37 @@ connection.connect(err => {
     console.log('Connected to the Employee Tracker database.');
     mainMenu();
 });
+
+// Main menu function
+function mainMenu() {
+    inquirer.prompt({
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+            'View All Departments',
+            'View All Roles',
+            'View All Employees',
+            'Add a Department',
+            'Add a Role',
+            'Add an Employee',
+            'Update an Employee Role',
+            'Exit'
+        ]
+    })
+    .then(answer => {
+        switch (answer.action) {
+            case 'View All Departments':
+                viewAllDepartments();
+                break;
+            // Add cases for other actions
+            case 'Exit':
+                connection.end();
+                break;
+            default:
+                console.log(`Invalid action: ${answer.action}`);
+                mainMenu();
+                break;
+        }
+    });
+}
